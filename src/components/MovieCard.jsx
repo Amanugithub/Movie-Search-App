b880ed706cdcd1a8e26105417b65cc6b9db2d5b5
Amanuel <amanuelget11@gmail.com>
2026-07-08 22:31:07 +0300

    improved information for images. add star sign for rating and genres for movies and shows

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,34 @@
+import { useContext } from "react";
+import { GenreContext } from "../contexts/GenreContext";
+import { FaStar } from "react-icons/fa";
+
 export default function MovieCard({movieDetails}){
     const IMG_BASE = "https://image.tmdb.org/t/p/w500";
+    const genres = useContext(GenreContext).genres;
+    const filteredGenres = genres.filter((obj)=>{ return movieDetails.genre_ids.includes(obj.id) });
+
+
     return (
         
-        <div className="embla__slide grow-0 shrink-0 flex-[0 0 100%] min-w-0 pl-(--slide-spacing) w-[25%] lg:w-[13%] ">
+        <div className="embla__slide grow-0 shrink-0 flex-[0 0 100%] min-w-0 pl-(--slide-spacing) w-[45%] lg:w-[20%] ">
             <div className=" bg-neutral-dark-700 border-2 border-neutral-dark-500 rounded-3xl border-dashed p-2">
                 <img src={`${IMG_BASE}${movieDetails.poster_path}`} alt="poster" className="
-                      w-50 rounded-3xl shadow-[0_0_10px_rgba(0,0,0,1)]" />
+                       rounded-3xl shadow-[0_0_10px_rgba(0,0,0,1)]" />
             </div>
+            
             <div className="info font-manrope text-secondary-dark-100 font-bold" >
                 <p>{movieDetails.title}</p>
-                <p className="text-sm ">{movieDetails.release_date.slice(0,4)} • <span className="rating text-primary-dark-100 " >{movieDetails.vote_average.toFixed(1)} Rating</span></p>
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center text-sm gap-1">
+                        <p className=" ">{movieDetails.release_date.slice(0,4)} •</p>
+                        <p>{filteredGenres[0].name}</p>
+                    </div>
+                    <div className="rating flex items-center text-primary-dark-100">
+                        <FaStar />
+                        <p className="pl-2" >{movieDetails.vote_average.toFixed(1)}</p>
+                    </div>
+                </div>
+                
             </div>
         </div>
             
